9767719533ccf32a56131568405f17d3d21daee3
adamsaparudin <adamsaparudin@gmail.com>
2017-03-11 11:06:51 +0700

    Refactor Code

--- 02. Refactor Code/js/script.js
+++ 02. Refactor Code/js/script.js
@@ -1,13 +1,25 @@
-$(document).ready( () => {
-  $('#roller button.add').on('click', () => {
-    console.log("WAT");
-    $('.dice').append('<div class="die">0</div>')
-  })
+// $(document).ready( () => {
+//   $('#roller button.add').on('click', () => {
+//     console.log("WAT");
+//     $('.dice').append('<div class="die">0</div>')
+//   })
+//
+//   $('#roller button.roll').on('click', () => {
+//     $('.die').each( (k, die) => {
+//       let value = Math.floor((Math.random() * 6) + 1)
+//       $('.die').text(value)
+//     })
+//   })
+// })
 
-  $('#roller button.roll').on('click', () => {
-    $('.die').each( (k, die) => {
-      let value = Math.floor((Math.random() * 6) + 1)
-      $('.die').text(value)
-    })
+
+function addDiceSlot() {
+  $('.dice').append('<div class="die">0</div>')
+}
+
+function rollDice() {
+  $('.die').each( (k, die) => {
+    let value = Math.floor((Math.random() * 6) + 1)
+    $('.die').text(value)
   })
-})
+}
