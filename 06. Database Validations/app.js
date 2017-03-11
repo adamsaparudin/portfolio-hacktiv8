f748ead857e0ad119a488e3c80e2850807b8a09b
adamsaparudin <adamsaparudin@gmail.com>
2017-03-11 16:39:10 +0700

    initial config

---
+++ 06. Database Validations/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/users', users);
+
+app.listen(3000)
+
+module.exports = app;
