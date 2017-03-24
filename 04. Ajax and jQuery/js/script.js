be7c18a534a99c3b1d5c4c9c26691d45918723b3
adamsaparudin <adamsaparudin@gmail.com>
2017-03-24 11:42:50 +0700

    Client scooping

--- 04. Ajax and jQuery/js/script.js
+++ 04. Ajax and jQuery/js/script.js
@@ -2,8 +2,6 @@ function getSetColor() {
   $.getJSON("http://www.colr.org/json/color/random", function(result) {
     let color = result.colors[0].hex
     let index = Math.floor(Math.random() * 9)
-    console.log(color)
-    console.log(index);
     $(".colorized").eq(index).css("background-color", "#"+color)
   })
 }
